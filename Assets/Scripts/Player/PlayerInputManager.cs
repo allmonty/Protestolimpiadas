af059d355ad25a0723d086a74e411b912a0e54c6
Allan <allanbrados@gmail.com>
2016-08-15 23:16:51 -0300

    Creates player with simple movement

---
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputManager : MonoBehaviour {
+
+    public PlayerMovement playerMove = null;
+    [Space(5)]
+    public string moveHorizontalAxis = "Horizontal";
+    public string moveVerticalAxis = "Vertical";
+
+    void Update()
+    {
+        readMovementInput();
+    }
+
+    void readMovementInput()
+    {
+        playerMove.move(Input.GetAxisRaw(moveHorizontalAxis), Input.GetAxisRaw(moveVerticalAxis));
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6f;
+
+	Rigidbody playerRigidbody;
+
+	void Awake()
+	{
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void move(float h, float v)
+	{
+        Vector3 movementDir = new Vector3(h, 0f, v);
+		movementDir = movementDir.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition(transform.position + movementDir);
+	}
+}
