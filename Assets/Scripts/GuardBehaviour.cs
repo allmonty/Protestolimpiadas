94d29b1f51021d8458f4889ae8fd2167cbcd0f0c
Miguel Péres <foxmpc@gmail.com>
2016-08-20 18:40:46 -0300

    Add patrolling behaviour to the guard; Guard loses player only after some specific time out of sight.

--- Assets/Scripts/GuardBehaviour.cs
+++ Assets/Scripts/GuardBehaviour.cs
@@ -5,20 +5,36 @@ public class GuardBehaviour : MonoBehaviour {
 
 	[SerializeField] float distanceToForget = 10;
 	[SerializeField] Transform target;
+
 	public bool chaseState;
+	public float timeToForget;
+	public float timer;
+	bool targetSpotted = false;
 
 	void Start () {
 		chaseState = false;	
+		timeToForget = 2.0F;
+		timer = 0.0F;
 	}
 
 	void Update () {
 		if (Vector3.Distance( this.transform.position, target.position) > distanceToForget )
 		{
-			chaseState = false;
+			if (targetSpotted)
+			{
+				timer += Time.deltaTime;
+				if (timer >= timeToForget)
+				{
+					chaseState = false;					
+					targetSpotted = false;
+					timer = 0.0F;
+				}				
+			}
 		}
 		else
 		{
 			chaseState = true;
+			targetSpotted = true;
 		}
 	}
 
--- Assets/Scripts/guardMovement.cs
+++ Assets/Scripts/guardMovement.cs
@@ -1,19 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class guardMovement : MonoBehaviour {
+public class GuardMovement : MonoBehaviour {
 
 	public Transform target;
+	public float waitingTime;
+	[SerializeField] Transform[] routeWaypoints;
 
 	NavMeshAgent agent;
+	GuardBehaviour guardBehaviour;
+	int nextWaypoint;
+	float timer = 0.0F;
+	float waypointThreshold = 0.01F;
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
+		guardBehaviour = GetComponent<GuardBehaviour>();
+
+		nextWaypoint = Random.Range (0, routeWaypoints.Length);
+		transform.position = routeWaypoints[nextWaypoint].position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		agent.destination = target.position;	
+		if (guardBehaviour.chaseState)
+		{
+			agent.destination = target.position;	
+		} else
+		{
+			agent.destination = routeWaypoints[nextWaypoint].position;
+			Vector3 normalizedWaypointPos = new Vector3 (agent.destination.x, transform.position.y, agent.destination.z);
+
+			if (Vector3.Distance (transform.position, normalizedWaypointPos) <= waypointThreshold)
+			{
+				transform.LookAt(transform.position + Vector3.back);
+
+				timer += Time.deltaTime;
+				if (timer >= waitingTime) {
+					timer = 0.0F;
+					nextWaypoint = Random.Range (0, routeWaypoints.Length);	
+				}
+			}
+		}
 	}
 }
