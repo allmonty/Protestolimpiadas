1c895f9a457bc185ce692074730b46187f79fa8a
Miguel Péres <foxmpc@gmail.com>
2016-08-20 01:05:59 -0300

    Add GuardBehaviour script and guard waypoints in the scene.

---
+++ Assets/Scripts/GuardBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardBehaviour : MonoBehaviour {
+
+	[SerializeField] float distanceToForget = 10;
+	[SerializeField] Transform target;
+	public bool chaseState;
+
+	void Start () {
+		chaseState = false;	
+	}
+
+	void Update () {
+		if (Vector3.Distance( this.transform.position, target.position) > distanceToForget )
+		{
+			chaseState = false;
+		}
+		else
+		{
+			chaseState = true;
+		}
+	}
+
+	// Gizmos
+	void OnDrawGizmosSelected() {
+		Gizmos.color = new Color(1.0F, 0.0F, 0.0F, 0.4F);
+		Gizmos.DrawSphere(transform.position, distanceToForget);
+	}
+}
