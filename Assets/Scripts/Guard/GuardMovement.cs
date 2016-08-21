c552d4a6fe78b0b21b2a43f3dc112f4ec6e7ad8a
Miguel Péres <foxmpc@gmail.com>
2016-08-21 13:30:39 -0300

    Add guard animation controller; Add speed parameters (for patrolling and chasing).

--- Assets/Scripts/Guard/GuardMovement.cs
+++ Assets/Scripts/Guard/GuardMovement.cs
@@ -7,40 +7,52 @@ public class GuardMovement : MonoBehaviour {
 	public float waitingTime;
 	[SerializeField] Transform[] routeWaypoints;
 
+	[Header("Speed Parameters")]
+	[SerializeField] float patrollingSpeed = 2.5F;
+	[SerializeField] float chaseSpeed = 4.0F;
+
 	NavMeshAgent agent;
 	GuardBehaviour guardBehaviour;
+	Animator anim;
+
 	int nextWaypoint;
 	float timer = 0.0F;
 	float waypointThreshold = 0.01F;
 
-	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		guardBehaviour = GetComponent<GuardBehaviour>();
+		anim = GetComponentInChildren<Animator>();
 
 		nextWaypoint = Random.Range (0, routeWaypoints.Length);
 		agent.Warp(routeWaypoints[nextWaypoint].position);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (guardBehaviour.chaseState) {
 			agent.Resume();
+			agent.speed = chaseSpeed;
 			agent.destination = target.position;	
+			anim.SetBool ("isChasing", true);
 		} 
 		else if (guardBehaviour.searchState)
 		{
 			agent.Stop();
+			anim.SetBool ("isChasing", false);
+			anim.SetBool ("isPatrolling", false);
 		}
 		else
 		{
 			agent.Resume();
 			agent.destination = routeWaypoints[nextWaypoint].position;
+			agent.speed = patrollingSpeed;
+			anim.SetBool ("isPatrolling", true);
 			Vector3 normalizedWaypointPos = new Vector3 (agent.destination.x, transform.position.y, agent.destination.z);
 
 			if (Vector3.Distance (transform.position, normalizedWaypointPos) <= waypointThreshold)
 			{
 				transform.LookAt(transform.position + Vector3.back);
+				anim.SetBool ("isPatrolling", false);
 
 				timer += Time.deltaTime;
 				if (timer >= waitingTime) {
