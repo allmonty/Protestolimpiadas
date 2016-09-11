c6473e3a8f9a2f4a7cbcc2a090970cd6641eb6ad
Allan <allanbrados@gmail.com>
2016-09-11 12:36:48 -0300

    Creates screen transition using vignette

---
+++ Assets/Scripts/Utility/SceneTransition_Vignetting.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition_Vignetting : MonoBehaviour {
+
+    public float openTimeDefault = 1f;
+    public float closeTimeDefault = 1f;
+
+    [Space(10)]
+
+    VignetteAndChromaticAberration targetVignette;
+
+    [SerializeField] float effectSpeed = 1.0f;
+    [SerializeField] float vignetting = 0f;
+    [SerializeField] int control = 0; //0 = stopped; 1 = opening; 2 = closing;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += sceneLoaded;
+    }
+
+    void sceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        targetVignette = Camera.main.GetComponent<VignetteAndChromaticAberration>();
+        if (targetVignette == null)
+        {
+            throw new System.Exception("The main camera needs vignette script!!");
+        }
+
+        openVignette();
+    }
+
+    void Update()
+    {
+        if (control == 1)//open
+        {
+            vignetting = Mathf.MoveTowards(vignetting, 0f, effectSpeed * Time.deltaTime);
+            targetVignette.intensity = vignetting;
+            if (vignetting <= 0f)
+            {
+                stopVignetting();
+            }
+        }
+        if (control == 2)//close
+        {
+            vignetting = Mathf.MoveTowards(vignetting, 1f, effectSpeed * Time.deltaTime);
+            targetVignette.intensity = vignetting;
+            if(vignetting >= 1f)
+            {
+                stopVignetting();
+            }
+        }
+    }
+
+    public void stopVignetting()
+    {
+        control = 0;
+    }
+
+    public void openVignette()
+    {
+        control = 1;
+        vignetting = 1f;
+        effectSpeed = 1f / openTimeDefault;
+    }
+
+    public void openVignette(float dtime)
+    {
+        control = 1;
+        vignetting = 1f;
+        effectSpeed = 1f / dtime;
+    }
+
+    public void closeVignette()
+    {
+        control = 2;
+        vignetting = 0f;
+        effectSpeed = 1f / closeTimeDefault;
+    }
+
+    public void closeVignette(float dtime)
+    {
+        control = 2;
+        vignetting = 0f;
+        effectSpeed = 1f / dtime;
+    }
+}
--- Assets/Scripts/Utility/Utility_PressButtonToChangeScene.cs
+++ Assets/Scripts/Utility/Utility_PressButtonToChangeScene.cs
@@ -5,14 +5,30 @@ using UnityEngine.SceneManagement;
 public class Utility_PressButtonToChangeScene : MonoBehaviour {
 
     public string nextSceneName = "Game";
-
     public string buttonName = "Submit";
+    public float delay = 1f;
+
+    SceneTransition_Vignetting transitionVignette;
+
+    void Start()
+    {
+        transitionVignette = GameObject.FindGameObjectWithTag("ScreenTransition").GetComponent<SceneTransition_Vignetting>();
+    }
 
 	void Update()
     {
         if(Input.GetButtonDown(buttonName))
         {
-            SceneManager.LoadScene(nextSceneName);
+            if(transitionVignette != null)
+            {
+                transitionVignette.closeVignette(delay);
+            }
+            Invoke("changeScene", delay);
         }
+    }
+
+    void changeScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
