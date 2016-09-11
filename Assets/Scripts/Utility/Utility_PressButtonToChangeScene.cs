91d2e9eba52e8d85bdda89c265b4b4639eae7821
Allan <allanbrados@gmail.com>
2016-09-11 10:28:49 -0300

    Creates start menu with animated "Press Enter" button

---
+++ Assets/Scripts/Utility/Utility_PressButtonToChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Utility_PressButtonToChangeScene : MonoBehaviour {
+
+    public string nextSceneName = "Game";
+
+    public string buttonName = "Submit";
+
+	void Update()
+    {
+        if(Input.GetButtonDown(buttonName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
