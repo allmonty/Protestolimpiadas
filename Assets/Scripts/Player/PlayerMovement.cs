3fc98046c1f4c0c4d265bb9082cb9f1d6aba4ef3
Allan <allanbrados@gmail.com>
2016-09-14 15:24:49 -0300

    Adjust some values in the player's animator

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,10 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public Transform stairLooker;
-
     public float speed = 6f;
-    public float stairJumpSpeed = 6f;
 
     PlayerHoldPoster playerHoldPoster;
 	Rigidbody playerRigidbody;
@@ -32,29 +29,10 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetBool("Running", false);
         }
 
-        float stairSpeedY = 0f;
-        if(isWalkingInStairs() && (h != 0f || v != 0f))
-        {
-            //stairSpeedY = stairJumpSpeed;
-        }
-
-        Vector3 movementDir = new Vector3(h, stairSpeedY, v);
+        Vector3 movementDir = new Vector3(h, 0f, v);
 		movementDir = movementDir.normalized * speed * Time.deltaTime;
 		playerRigidbody.MovePosition(transform.position + movementDir);
 	}
-
-    bool isWalkingInStairs()
-    {
-        RaycastHit rayHit;
-        if (Physics.Raycast(stairLooker.position, stairLooker.forward, out rayHit, 1f, 1 - LayerMask.NameToLayer("Stairs")))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
 
     public void look(float x, float y)
     {
