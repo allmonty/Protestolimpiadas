72de3b534b4eadffaa2bb662e9983e87274483cd
Allan <allanbrados@gmail.com>
2016-09-15 22:32:27 -0300

    Import GameJolt API and puts it on start menu

---
+++ Assets/SignInGamejolt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignInGamejolt : MonoBehaviour {
+
+    public Utility_PressButtonToChangeScene sceneChanger;
+
+    void Start()
+    {
+        bool isSignedIn = GameJolt.API.Manager.Instance.CurrentUser != null;
+        if(isSignedIn)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+	public void openUIToSign()
+    {
+        bool isSignedIn = GameJolt.API.Manager.Instance.CurrentUser != null;
+
+        if(!isSignedIn)
+        {
+            GameJolt.UI.Manager.Instance.ShowSignIn();
+            sceneChanger.enabled = false;
+        }
+    }
+
+    public void reactivateSceneChanger()
+    {
+        sceneChanger.enabled = true;
+    }
+}
