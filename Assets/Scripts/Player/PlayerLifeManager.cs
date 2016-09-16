a57f4cc70b26f042ae657eee4ebf2b728605ff1b
Allan <allanbrados@gmail.com>
2016-09-15 21:42:14 -0300

    Fix life hud problems of null reference

--- Assets/Scripts/Player/PlayerLifeManager.cs
+++ Assets/Scripts/Player/PlayerLifeManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLifeManager : MonoBehaviour {
 
-    [SerializeField] GameObject[] lifeImages;
+    GameObject[] lifeImages = new GameObject[3];
 
     Utility_ChangeScene sceneChanger;
     int life = 3;
@@ -12,6 +12,11 @@ public class PlayerLifeManager : MonoBehaviour {
     void Start()
     {
         sceneChanger = Camera.main.GetComponent<Utility_ChangeScene>();
+        GameObject HUDLife = GameObject.Find("HUD Life");
+        Transform lives = HUDLife.transform.GetChild(0);
+        lifeImages[0] = lives.GetChild(0).gameObject;
+        lifeImages[1] = lives.GetChild(1).gameObject;
+        lifeImages[2] = lives.GetChild(2).gameObject;
     }
 
 	public void takeDamage(int dmg)
